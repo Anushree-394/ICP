f4986adaf0dd378d111c824d50ce6636c93c10d6
Anushree-394 <anushreeagrawal41@gmail.com>
2025-10-24 23:52:24 +0530

    done

--- Daily_Class_work/Day_6/reverse_string.java
+++ Daily_Class_work/Day_6/reverse_string.java
@@ -3,7 +3,24 @@ import java.util.*;
 public class reverse_string {
     public static void main(String[] main){
         Scanner sc=new Scanner(System.in);
-        String s=sc.next();
-        
+        int n=sc.nextInt();
+        char s[] = new char[n];
+        for (int i = 0; i < n; i++) {
+            s[i] = sc.next().charAt(0);
+        }
+        reverse(s, 0, 0);
+        sc.close();
     }
+
+    public static void reverse(char[] s, int i, int j) {
+        if (i >= j) {
+            return;
+        }
+        char a = s[i];
+        s[i] = s[j];
+        s[j] = a;
+        return;
+    }
+        
+    
 }
